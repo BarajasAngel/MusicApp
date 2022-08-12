772656ce4cdcc96a9eeed2b0e7bc66c71ae2357a
Angel Barajas <Barajasb15@gmail.com>
2022-08-11 20:20:12 -0500

    add: Navbar del inicio

--- MusicApp/Controllers/HomeController.cs
+++ MusicApp/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace MusicApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
