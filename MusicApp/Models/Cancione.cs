eed6348e700df7ca510cdf444c3967b60ac6ecec
Angel Barajas <Barajasb15@gmail.com>
2022-08-12 20:47:53 -0500

    add: disño del modal de agregar

---
+++ MusicApp/Models/Cancione.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicApp.Models
+{
+    public partial class Cancione
+    {
+        [Key]
+        public int IdCancion { get; set; }
+        [Display(Name = "Link:")]
+        public string Link { get; set; } = null!;
+        [Display(Name = "Titulo:")]
+        [Required(ErrorMessage = "Ingresa un titulo")]
+        public string Titulo { get; set; } = null!;
+        [Display(Name = "Grupo:")]
+        [Required(ErrorMessage = "Ingresa un grupo")]
+        public string Grupo { get; set; } = null!;
+        [Display(Name = "Año:")]
+        [Required(ErrorMessage = "Ingresa un año")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "El Año debe tener 4 caracteres")]
+        public string Año { get; set; } = null!;
+        [Display(Name = "Genero:")]
+        [Required(ErrorMessage = "Ingresa un genero")]        
+        public string Genero { get; set; } = null!;
+    }
+}
---
+++ MusicApp/Models/MusicAppDBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MusicApp.Models
+{
+    public partial class MusicAppDBContext : DbContext
+    {
+        public MusicAppDBContext()
+        {
+        }
+
+        public MusicAppDBContext(DbContextOptions<MusicAppDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cancione> Canciones { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=Asura;Initial Catalog=MusicAppDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cancione>(entity =>
+            {
+                entity.HasKey(e => e.IdCancion)
+                    .HasName("PK__Cancione__14F1523ABAC895DB");
+
+                entity.Property(e => e.IdCancion).HasColumnName("Id_Cancion");
+
+                entity.Property(e => e.Año).HasMaxLength(4);
+
+                entity.Property(e => e.Genero).HasMaxLength(30);
+
+                entity.Property(e => e.Grupo).HasMaxLength(30);
+
+                entity.Property(e => e.Titulo).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
